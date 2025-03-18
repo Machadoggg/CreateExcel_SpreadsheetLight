563e8e16aafe389dca52755b523caa6e1feee4ca
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-18 17:26:57 -0500

    Add Rows

--- CreateExcel_SpreadsheetLight/Program.cs
+++ CreateExcel_SpreadsheetLight/Program.cs
@@ -12,3 +12,9 @@ dt.Columns.Add("Name", typeof(string));
 dt.Columns.Add("Age", typeof(int));
 dt.Columns.Add("Gender", typeof(string));
 
+//Add Rows
+dt.Rows.Add("Pedro", 13, "Hombre");
+dt.Rows.Add("Ana", 23, "Mujer");
+dt.Rows.Add("Luis", 33, "Hombre");
+dt.Rows.Add("Lina", 13, "Mujer");
+dt.Rows.Add("Leon", 43, "Hombre");
