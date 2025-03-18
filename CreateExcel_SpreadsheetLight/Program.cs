21ba0e72a837bd92d2ff6a653729f31d160db6d1
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-18 17:26:24 -0500

    Add Columns

--- CreateExcel_SpreadsheetLight/Program.cs
+++ CreateExcel_SpreadsheetLight/Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-using SpreadsheetLight;
+﻿using SpreadsheetLight;
 
 string pathFile = AppDomain.CurrentDomain.BaseDirectory + "myExcel.xlsx";
 
@@ -10,4 +7,8 @@ SLDocument oSLDocument = new SLDocument();
 System.Data.DataTable dt = new System.Data.DataTable();
 
 
+//Add Columns
+dt.Columns.Add("Name", typeof(string));
+dt.Columns.Add("Age", typeof(int));
+dt.Columns.Add("Gender", typeof(string));
 
